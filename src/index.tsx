cc7410f4bcd72a4265c1f78e5ff2dcd7ce4b13e9
Philipp A <flying-sheep@web.de>
2022-11-17 14:06:35 +0100

    Use route matching instead of static pathList

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import { css, jsx } from '@emotion/react';
-import { useEffect, useMemo, useRef, cloneElement, Children, ReactElement } from 'react';
+import { useMemo, useRef, cloneElement, Children, ReactElement } from 'react';
 import t from 'prop-types';
-import { useLocation, Routes } from 'react-router-dom';
+import { useLocation, useRoutes, createRoutesFromChildren, matchRoutes, RouteObject } from 'react-router-dom';
 import { CSSTransition, TransitionGroup } from 'react-transition-group';
 
 type Direction = 'forward' | 'back'
@@ -124,72 +124,18 @@ const getCss = (
   }
 `;
 
-const CACHE_KEY = '::slide::history::';
+const findRouteIndex = (routes: RouteObject[], pathname: string): number => {
+  const matches = matchRoutes(routes, pathname);
+  if (matches === null) throw new Error(`Route ${pathname} does not match`)
+  return routes.findIndex((route) => matches.some((match) => route === match.route))
+}
 
-const SlideRoutes = ({ animation, pathList, duration, timing, destroy, children }: SlideRoutesProps) => {
+const SlideRoutes = ({ animation, duration, timing, destroy, children }: SlideRoutesProps) => {
   const location = useLocation();
   const { pathname } = location;
 
-  const hasMount = useRef(false);
-  const pathQueue = useRef<string[]>();
-  const SHOULD_UPDATE_CACHE = useRef(false);
-
   const prevPath = useRef<string>();
   const direction = useRef<Direction>();
-
-  if (!hasMount.current) {
-    // mount
-    hasMount.current = true;
-
-    if (pathList.length > 0) {
-      prevPath.current = pathname;
-    } else {
-      const cacheList = sessionStorage.getItem(CACHE_KEY);
-      if (!cacheList) {
-        prevPath.current = pathname;
-        pathQueue.current = [pathname];
-        SHOULD_UPDATE_CACHE.current = true;
-      } else {
-        pathQueue.current = JSON.parse(cacheList);
-        prevPath.current = pathQueue.current![pathQueue.current!.length - 1];
-      }
-    }
-  } else {
-    // update
-    if (prevPath.current !== pathname) {
-      if (pathList.length > 0) {
-        const prevIndex = pathList.indexOf(prevPath.current!);
-        const nextIndex = pathList.indexOf(pathname);
-        direction.current = prevIndex < nextIndex ? 'forward' : 'back';
-      } else {
-        const nextIndex = pathQueue.current!.lastIndexOf(pathname);
-
-        if (nextIndex === -1) {
-          direction.current = 'forward';
-          pathQueue.current!.push(pathname);
-        } else {
-          direction.current = 'back';
-          pathQueue.current!.length = nextIndex + 1;
-        }
-
-        SHOULD_UPDATE_CACHE.current = true;
-      }
-
-      prevPath.current = pathname;
-    }
-  }
-
-  const cssProps = useMemo(
-    () => (destroy ? { timeout: duration } : { addEndListener() {} }),
-    [destroy, duration]
-  );
-
-  useEffect(() => {
-    if (SHOULD_UPDATE_CACHE.current) {
-      SHOULD_UPDATE_CACHE.current = false;
-      sessionStorage.setItem(CACHE_KEY, JSON.stringify(pathQueue.current));
-    }
-  });
 
   const routeList = useMemo(() => {
     return Children.map(children, (child) => {
@@ -203,14 +149,29 @@ const SlideRoutes = ({ animation, pathList, duration, timing, destroy, children 
     });
   }, [children]);
 
+  const routes = useMemo(() => createRoutesFromChildren(routeList), [routeList]);
+  const routesElement = useRoutes(routes, location);
+  
+  if (prevPath.current && prevPath.current !== pathname) {
+    const prevIndex = findRouteIndex(routes, prevPath.current);
+    const nextIndex = findRouteIndex(routes, pathname);
+    direction.current = prevIndex < nextIndex ? 'forward' : 'back';
+  }
+  prevPath.current = pathname;
+
+  const cssProps = useMemo(
+    () => (destroy ? { timeout: duration } : { addEndListener() {} }),
+    [destroy, duration]
+  );
+
   return (
     <TransitionGroup
       className={`slide-routes ${animation}`}
-      childFactory={(child: ReactElement) => cloneElement(child, { classNames: direction.current })}
+      childFactory={(child) => cloneElement(child, { classNames: direction.current })}
       css={getCss(duration, timing, direction.current!)}
     >
       <CSSTransition key={pathname} {...cssProps}>
-        <Routes location={location}>{routeList}</Routes>
+        {routesElement}
       </CSSTransition>
     </TransitionGroup>
   );
@@ -226,7 +187,6 @@ SlideRoutes.defaultProps = {
 
 SlideRoutes.propTypes = {
   animation: t.oneOf(['slide', 'vertical-slide', 'rotate'] as SlideRoutesProps['animation'][]),
-  pathList: t.arrayOf(t.string.isRequired),
   duration: t.number,
   timing: t.oneOf(['ease', 'ease-in', 'ease-out', 'ease-in-out', 'linear'] as SlideRoutesProps['timing'][]),
   destroy: t.bool,
@@ -235,7 +195,6 @@ SlideRoutes.propTypes = {
 
 export interface SlideRoutesProps {
   animation: 'slide' | 'vertical-slide' | 'rotate',
-  pathList: string[],
   duration: number,
   timing: 'ease' | 'ease-in' | 'ease-out' | 'ease-in-out' | 'linear',
   destroy: boolean,
