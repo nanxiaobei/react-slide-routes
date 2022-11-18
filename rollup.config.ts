59a3fff1c693435cd394df8b5e83980451025d6c
nanxiaobei <nanxiaobei@gmail.com>
2022-11-19 06:18:58 +0800

    update configs

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,16 +1,17 @@
+import { readFileSync } from 'fs';
 import typescript from '@rollup/plugin-typescript';
 import dts from 'rollup-plugin-dts';
-import pkg from './package.json';
+
+const pkg = JSON.parse(readFileSync('./package.json') as unknown as string);
 
 const input = 'src/index.tsx';
-const external = () => true;
-
 const cjsOutput = { file: pkg.main, format: 'cjs', exports: 'auto' };
 const esmOutput = { file: pkg.module, format: 'es' };
 const dtsOutput = { file: pkg.types, format: 'es' };
+const external = () => true;
 
 export default [
-  { input, output: cjsOutput, external, plugins: [typescript()] },
-  { input, output: esmOutput, external, plugins: [typescript()] },
+  { input, output: cjsOutput, plugins: [typescript()], external },
+  { input, output: esmOutput, plugins: [typescript()], external },
   { input, output: dtsOutput, plugins: [dts()] },
 ];
